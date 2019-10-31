a54c10d5c098e5211501e51796dc9bb62f87bbb2
EmreDurmus <emredurmus@EresAdmins-MacBook-Pro.local>
2019-10-31 15:59:26 -0400

    dsadsadasdsdss

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<h1>Hello World</h1>');
-  console.log(req)
+  console.log(req.headers)
 });
 
 server.listen(port,() => {
